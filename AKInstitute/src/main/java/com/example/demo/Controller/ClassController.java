ee26604e60bb879e7e0be0238f46a0c69970237f
pawanmalokar296-max <pawanmalokar296@gamil.com>
2025-11-09 12:47:34 +0530

    add Get Controller

--- AKInstitute/src/main/java/com/example/demo/Controller/ClassController.java
+++ AKInstitute/src/main/java/com/example/demo/Controller/ClassController.java
@@ -1,5 +1,17 @@
 package com.example.demo.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ClassController {
 
+	      @GetMapping("hello")
+	      public void main() {
+	    	  
+	    	  
+	    	   System.out.println("hello word");
+	    	  
+	      }
+	
 }
